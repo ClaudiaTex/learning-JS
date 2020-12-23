ebc5db6c076b8128d7142760ebea3607ac5e2fa4
Claudia Texidor <claudiatexidor@hotmail.com>
2020-12-23 11:47:22 +0100

    Learning about arrays

--- class1JS.js
+++ class1JS.js
@@ -4,3 +4,10 @@ var name = "pedro"; //string
 let friend = true; //boolean
 
 const age = 28; //number
+
+//ARRAYS
+
+let array = ["pedro", "pablo", 18, 25, true];
+
+console.log(array);
+console.log(array.length);
