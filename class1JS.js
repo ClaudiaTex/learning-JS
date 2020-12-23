33a6f92d5edb781cfdfb9b2a55d34dd96b4e351b
Claudia Texidor <claudiatexidor@hotmail.com>
2020-12-23 11:48:59 +0100

    Learning how to loop through an array with FOR

--- class1JS.js
+++ class1JS.js
@@ -11,3 +11,45 @@ let array = ["pedro", "pablo", 18, 25, true];
 
 console.log(array);
 console.log(array.length);
+
+//FOR: loop through an array
+console.warn("FOR");
+
+for (let i = 0; i < 5; i++) {
+  console.log(array[i]);
+}
+
+for (let i = 0; i < array.length; i++) {
+  console.log("Im learning");
+  console.log(array[i]);
+}
+
+//FOR IN
+console.warn("FOR IN");
+
+let people = ["pedro", "pablo", "maria"];
+
+for (let person in people) {
+  console.log(people[person]);
+}
+
+//FOR OF
+console.warn("FOR OF");
+
+let persons = ["pedro", "pablo", "maria"];
+
+for (let person of persons) {
+  console.log(person);
+}
+
+//LOOP THROUGH ONLY HALF ARRAY
+
+console.warn("HALF ARRAY FOR");
+
+let persons2 = ["pedro", "pablo", "maria"];
+let halfloop = persons2.length / 2;
+
+for (let i = 0; i < halfloop; i++) {
+  console.log("Im learning loops");
+  console.log(persons2[i]);
+}
