8cc2b2921b28443e153ece044fd1f9bad4b76c38
Claudia Texidor <claudiatexidor@hotmail.com>
2021-01-05 18:37:39 +0100

    Learning how to use classes to create multiple objects with a constructor

--- class2JS.js
+++ class2JS.js
@@ -36,3 +36,36 @@ console.warn("JS OBJECT");
 
 let personObj = JSON.parse(personJson);
 console.log(personObj);
+
+//USING CLASSES TO CREATE MULTIPLE OBJECTS QUICKLY WITH A CONSTRUCTOR
+console.warn("CONSTRUCTING OBJECTS BY CLASS");
+
+class player {
+  constructor(name, age, height) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+  }
+}
+//using this. to created a new key
+
+let alejandro = new player("Alejandro", 29, "183cm");
+let victor = new player("Victor", 29, "173cm");
+//the order of the values ​​is the same as we originally created the keys
+
+console.log(alejandro);
+console.log(victor);
+
+//Other example
+class team {
+  constructor(name, city, year) {
+    this.name = name;
+    this.city = city;
+    this.year = year;
+  }
+}
+
+let atleticoMadrid = new team("Atletico de Madrid", "Madrid", 1903);
+
+console.log(atleticoMadrid);
+//check that the order of keys showing on the console browser is alphabetically
