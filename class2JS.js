f0e522c0aa75db1081301bcb66110729f394701f
Claudia Texidor <claudiatexidor@hotmail.com>
2021-01-05 17:28:47 +0100

    Nested object example

---
+++ class2JS.js
@@ -0,0 +1,25 @@
+/*EXAMPLE OF NESTED OBJECT*/
+
+let person = {
+  name: "Alejandro",
+  age: 29,
+  last_name: "Martin",
+  city: "Madrid",
+  favorite_games: [
+    {
+      name: "Fifa 20",
+      type: "Sports",
+    },
+    {
+      name: "NBA 2020",
+      type: "Sports",
+    },
+    {
+      name: "Fornite",
+      type: "Battle Royale",
+    },
+  ],
+  car: true,
+  model: "Polo",
+};
+console.log(person);
