bc0697521ebb7c0335da78eb9684e0dba610f54f
Claudia Texidor <claudiatexidor@hotmail.com>
2021-01-05 17:39:30 +0100

    Object to JSON / JSON to Object

--- class2JS.js
+++ class2JS.js
@@ -23,3 +23,13 @@ let person = {
   model: "Polo",
 };
 console.log(person);
+
+/*JSON: HOW TO TRANSFORM AN OBJECT IN TO JSON TO SEND INFORMATION QUICKLY*/
+
+let personJson = JSON.stringify(person);
+console.log(personJson);
+
+/*JSON: HOW TO TRANSFORM A JSON IN TO A JS OBJECT*/
+
+let personObj = JSON.parse(personJson);
+console.log(personObj);
