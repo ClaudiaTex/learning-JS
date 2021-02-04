8aaf69ec86a480750cf5fcb8d17fe9d0f32c7596
ClaudiaTexidor <claudiatexidor@hotmail.com>
2021-02-04 19:47:39 +0100

    Learning how to use IF conditional

--- class3JS.js
+++ class3JS.js
@@ -1,5 +1,13 @@
 //Use prompt to make an alert and ask to the user to introduce some data
-let names = prompt('Introduce your name');
+let name = prompt('Introduce your name');
 
-console.log(names)
+console.log(name)
+
+// CONDITIONAL : IF
+
+if (name.length > 5) {
+        alert('Your name has more than 5 letters') //this code only will run if the condition is met
+}
+
+//in this case .length counts all the chactersof a string including spaces
 
