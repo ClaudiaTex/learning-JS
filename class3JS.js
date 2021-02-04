f9fd49f298c6cf39444c8edceeb219233619920b
ClaudiaTexidor <claudiatexidor@hotmail.com>
2021-02-04 20:16:36 +0100

    Learning how to use NESTED IF-ELSE

--- class3JS.js
+++ class3JS.js
@@ -13,11 +13,11 @@ console.log(name)
 
 // CONDITIONAL : IF-ELSE
 
-if (name.length > 5) {
-    alert('Your name has more than 5 letters') //if this condition is not met, it will go to the next one
-} else {
-    alert('Your name has less than 5 letters')
-}
+// if (name.length > 5) {
+//     alert('Your name has more than 5 letters') //if this condition is not met, it will go to the next one
+// } else {
+//     alert('Your name has less than 5 letters')
+// }
 
 //But, what if the name is just 5 letters long? it will go to: 'Your name has less than 5 letters'
 // We can use >= (greater than or equal) on the if condition: if (name.length >= 5)
@@ -25,4 +25,19 @@ if (name.length > 5) {
 /* Note that the code is repeated, comment the IF section to see correctly the use of IF-ELSE
  on the console of your browser.*/
 
- 
+ // NESTED IF-ELSE
+
+if(name === 'Alex') {
+    console.log('Hello Alex');
+    if(name === 5){
+        console.log('Your name has 5 letters');
+    }else {
+        console.log ('Your name has 4 letters')
+    }
+}else {
+    console.log('You are not Alex')
+}
+
+/* Note that the code is repeated, comment the IF-ELSE section to see correctly the use of nested 
+ IF-ELSE on the console of your browser.
+*/
