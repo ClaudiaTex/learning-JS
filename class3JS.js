bb43197623f3d3d2775457a9e173152ced53b8ef
ClaudiaTexidor <claudiatexidor@hotmail.com>
2021-02-08 19:14:01 +0100

    Learning how to use SWITCH: example 2

--- class3JS.js
+++ class3JS.js
@@ -1,7 +1,8 @@
 //Use prompt to make an alert and ask to the user to introduce some data
-let name = prompt('Introduce your name');
 
-console.log(name)
+// let name = prompt('Introduce your name');
+
+// console.log(name)
 
 // CONDITIONAL : IF
 
@@ -9,7 +10,7 @@ console.log(name)
 //     alert('Your name has more than 5 letters') //this code only will run if the condition is met
 // }
 
-//in this case .length counts all the chactersof a string including spaces
+//in this case .length counts all the chacters of a string including spaces
 
 // CONDITIONAL : IF-ELSE
 
@@ -74,18 +75,62 @@ switch (key) {
 
 //EXAMPLE 1:
 
-switch (name) { //variable that we want to access
-    case 'Alejandro':
-        console.log(`Hello ${name}`); 
-        // to convert variables to a string: blacktips (``), $ and open {}
-        break;
-    case 'Cesar':
-        console.log(`Hola ${name}`);
-        break;
-    case 'Lola':
-        console.log(`Hola ${name}`);
-        break;
-    default:
-        console.log('Hello world')
-        break;
+// switch (name) { //variable that we want to access
+//     case 'Alejandro':
+//         console.log(`Hello ${name}`); 
+//         // to convert variables to a string: blacktips (``), $ and open {}
+//        break;
+//      case 'Cesar':
+//         console.log(`Hola ${name}`);
+//         break;
+//     case 'Lola':
+//         console.log(`Hola ${name}`);
+//         break;
+//     default:
+//         console.log('Hello world')
+//         break;
+// }
+
+/* Note that the code is repeated, comment the ELSE-IF section to see correctly 
+the use of SWITCH on the console of your browser.
+*/
+
+//EXAMPLE 2: Object from database (process)
+
+//STEP 1: Array of objects
+
+let videogames = [
+    {name:'play'},
+    {name:'TV'},
+    {name:'remote control'}
+];
+
+//STEP 2: We use a FOR (we go trough a loop, one turn for each object)
+for(let videogame of videogames) { //the simplest FOR to use
+    //STEP 3: we create a switch inside
+    switch (videogame.name) { 
+        case 'Romeo':
+            console.log('I am Romeo')
+            break;
+        case 'TV':
+            console.log('There are a TV')
+            break;
+        case 'remote control':
+            console.log('There are a remote control')
+            break;
+        default:
+            console.log('I am not here')
+            break;
+    }
 }
+// The console browser will show us the objects that are fulfilled (I am not here, There are a TV, There are a remote control)
+
+// Inside of a SWITCH case we can put whatever we need, for example a function or an other switch. 
+// Remember that: Default appears when no case matches.
+
+/* Comment the EXAMPLE 1 section to see correctly the EXAMPLE 2 of SWITCH on the console of your browser. 
+You can comment too the prompt of the beginning to not interfere with your practice of SWITCH.
+*/
+
+
+// "IF-ELSE / SWITCH": We use them when we know that the result will be true or false.
