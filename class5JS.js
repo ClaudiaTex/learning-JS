8c176f7c65d247d04513eae88359ca498a428d92
ClaudiaTexidor <claudiatexidor@hotmail.com>
2021-02-18 19:25:23 +0100

    Learning how a forEach works

--- class5JS.js
+++ class5JS.js
@@ -24,7 +24,7 @@ let Bike = new product('Bike-BTT', 'ROCKRIDER', 200, 'Sports');
 let Newspaper = new product('New York Times', 'NYT', 2, 'Press');
 
 console.warn('Object');
-console.log(Axe)
+console.log(Axe);
 
 //Put the objects inside an array
 
@@ -33,4 +33,29 @@ let products = [Xbox, Axe, IceBlue, Iphone_X, IMac, Ball, Bike, Newspaper];
 console.warn('Array of products');
 console.log(products);
 
-/*----- Methods to loop through an array -----*/
+/*----- Methods to loop through an array without using FOR-----*/
+
+// forEach: it's like a normal FOR but with different syntax (simpler)
+console.warn('FOR EACH');
+
+console.warn('Index position by product');
+products.forEach(function(product, index){ 
+    // INDEX in case you need to know what specific position the array objects occupy
+    product.price = product.price + 100;
+    console.log(index, product);
+});
+
+console.warn('Product price + 100');
+console.log(products);
+//It will show us the objects adding 100 to the price of each object
+
+//By working as a normal FOR, we can include conditionals inside {}.
+
+products.forEach(function(product){ 
+    if (product.type === 'Hygiene') {
+        product.origin = "EEUU";
+    }
+});
+
+console.warn('Hygiene products are from EEUU');
+console.log(products);
